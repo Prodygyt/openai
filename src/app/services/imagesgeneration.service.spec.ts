8014cb2f55b3837833b2e5389e40dc669e9fcb77
Prodygyt <urbinaaaron2000@gmail.com>
2023-03-19 21:42:42 -0600

    update xits

--- src/app/services/imagesgeneration.service.spec.ts
+++ src/app/services/imagesgeneration.service.spec.ts
@@ -10,7 +10,7 @@ describe('ImagesgenerationService', () => {
     service = TestBed.inject(ImagesgenerationService);
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
   });
 });
