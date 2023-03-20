1651dcbb7b61ea3d7a636d06e88201b15600c24d
Prodygyt <urbinaaaron2000@gmail.com>
2023-03-19 21:01:05 -0600

    update karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -33,14 +33,19 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
     browsers: ['Chrome'],
     browsers: ['ChromeHeadlessCI'],
-      customLauncher: {
-        ChromeHeadlessCI: {
-          base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-        }
-      },
+        customLaunchers: {
+          ChromeHeadlessCI: {
+           base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+         }
+    },
+    singleRun: false,
     restartOnFileChange: true
   });
 };
